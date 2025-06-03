577db7e328a250d6a216cbe4cfe003a4475eb373
Neel Rajani <97137109+Neelectric@users.noreply.github.com>
2025-06-03 18:21:46 +0100

    Update metrics_sample.py

--- src/lighteval/metrics/metrics_sample.py
+++ src/lighteval/metrics/metrics_sample.py
@@ -935,11 +935,11 @@ class JudgeLLM:
 class JudgeLLMSimpleQA(JudgeLLM):
     def __init__(self):
         super().__init__(
-            judge_model_name="chatgpt-4o-latest",
+            judge_model_name="gpt-4.1-2025-04-14",
             template=get_judge_prompt_simpleqa,
             process_judge_response=process_judge_response_simpleqa,
             judge_backend="openai",
-            short_judge_name="chatgpt-4o-latest",
+            short_judge_name="gpt-4.1",
         )
 
     def compute(self, sample_ids: list[str], responses: list, formatted_docs: list[Doc], **kwargs) -> dict[str, float]:
